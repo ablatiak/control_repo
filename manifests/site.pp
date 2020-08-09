aa4184d7ce30c5cff3d618c280ab914914837dc5
ablatiak <56833398+ablatiak@users.noreply.github.com>
2020-08-09 22:53:17 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,6 +7,9 @@ node 'localhost.localdomain' {
     content => "Welcome to ${fqdn}\n",
   }
 }
+node 'mintest.puppet.vm' {
+  include role::minecraft_server
+}
 node /^web/ {
   include role::app_server
 }
