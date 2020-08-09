22746ba4866ef3444675f7929731c39f47c77e75
ablatiak <56833398+ablatiak@users.noreply.github.com>
2020-08-09 19:52:36 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDkpUpTRMCMHeYoSjyUtKKXLy+XTdFubNxl9BlMU9Rrc8n898lNN4s07kD3iHNv1l44f9Nw+bw/ISd/U1HSFH1Lx1/CeJ5K1VeY/O3OHAP1QcczimUSPobSVcIilrCWuoryDRALCWENbq0v27rXHmmBcA9Qytvrn8XggYqqZLlnQi9FKOLn7zM50aomLmFku0L7d9jpkjgK8oOeJaTrdY1p+MNwcS0p+k+qRuf8NitUagtxqnfTkFuBBtZtHRTAJhY2la9qHguFAWo4QHUbkKs/UknjeBtyWTnXM8OGaj6bU3h5Md7h/nYxhiTRtylCqVkVZ//MkZznfYbglud+20r1',
 	}  
 }
